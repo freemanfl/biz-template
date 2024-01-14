37795e99f7d6f6b3f0c1c5d7c985272ddf714fce
Farid Mammadbayov <mm.farid@post.cz>
2024-01-14 18:36:43 +0100

    settings init

---
+++ settings.js
@@ -0,0 +1,24 @@
+const settings = {
+    name: "Bulldog Steak House",
+    type: "restaurant",
+    address: "Pisecna 2990/3",
+    themes: [{
+        light: {
+            main: '#232323',
+        }
+    },
+    {
+        dark: {
+            main: '#445523',
+        }
+    }
+
+    ],
+    navigation: ["Home", "Menu", "About us", "Contact", "Blog"],
+    menuCategories: ["Drinks", "Main courses", "Vine", "XXL Steaks"],
+    menu: [
+        {
+            category: "Drinks",
+            items:}, [], [], [],
+    ]
+}
